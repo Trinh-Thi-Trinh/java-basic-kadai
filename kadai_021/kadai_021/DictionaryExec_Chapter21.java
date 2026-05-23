bde192ba7d51dc20c5815e681cf5f19d5ed674cc
Trinh-Thi-Trinh <trinhtrinh519@gmail.com>
2026-05-23 19:10:55 +0900

    Kadai 21 Java

---
+++ kadai_021/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,11 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21  dictionary1 = new Dictionary_Chapter21();
+		String[] words = {"apple","banana","grape","orange"};
+		dictionary1.searchWord(words);
+	}
+
+}
